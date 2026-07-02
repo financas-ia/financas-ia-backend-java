87c2ca41f186d6c7f72ca4720552fb02ebd286f3
Igor Reis <igorreis377@gmail.com>
2026-07-02 12:52:04 -0300

    test: update test properties for H2 database and enhance JWT secrets

--- src/test/java/com/example/financas/FinancasApplicationTests.java
+++ src/test/java/com/example/financas/FinancasApplicationTests.java
@@ -4,16 +4,21 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;MODE=PostgreSQL",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+
         "MAIL_HOST=localhost",
         "MAIL_PORT=25",
         "MAIL_USER=mock",
         "MAIL_PASS=mock",
-        "DB_URL=jdbc:postgresql://localhost:5432/mock",
-        "DB_USER=mock",
-        "DB_PASS=mock",
-        "acess_jwt_secret=mockSecret123mockSecret123mockSecret123",
-        "refresh_jwt_secret=mockSecret123mockSecret123mockSecret123",
-        "pre_auth_token=mockSecret123mockSecret123mockSecret123",
+
+        "acess_jwt_secret=ChaveSecretaSuperSeguraComMaisDe32CaracteresTexto!",
+        "refresh_jwt_secret=ChaveSecretaSuperSeguraComMaisDe32CaracteresTextoRefresh!",
+        "pre_auth_token=ChaveSecretaSuperSeguraComMaisDe32CaracteresTextoPreAuth!",
         "GEMINI_API_KEY=mock"
 })
 class FinancasApplicationTests {
